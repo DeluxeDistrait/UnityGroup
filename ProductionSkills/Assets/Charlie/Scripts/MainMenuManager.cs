1a15e33137980d9733efdf1cf3077d830fdd80c1
CharlieM-Y <charliemurdochyardley@gmail.com>
2025-04-15 23:52:33 +1000

    Final Main Menu edit

--- ProductionSkills/Assets/Charlie/Scripts/MainMenuManager.cs
+++ ProductionSkills/Assets/Charlie/Scripts/MainMenuManager.cs
@@ -26,4 +26,9 @@ public class MainMenuManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
 }
